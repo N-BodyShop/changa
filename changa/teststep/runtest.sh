157ec034b45517e322a6ce80bf3bb6511ca72d87
Tom Quinn <trq@astro.washington.edu>
2007-01-11 18:37:42 +0000

    Updated to use "ChaNGa"

--- changa/teststep/runtest.sh
+++ changa/teststep/runtest.sh
@@ -1,9 +1,9 @@
 # simple test of integrator
 # check for lines with "Energy" in the output
-# run Parallel gravity for 100 timesteps of .01.  This is roughly
+# run ChaNGa for 100 timesteps of .01.  This is roughly
 # one dynamical time in the center of the cluster.
 # compare this with numbers in "pkdtest.log"
 
-../charmrun +p 2 ../ParallelGravity -v 1 -p 2 test_pg.param
+../charmrun +p 2 ../ChaNGa -v 1 -p 2 test_pg.param
 
 # pkdgrav test_pkd.param
