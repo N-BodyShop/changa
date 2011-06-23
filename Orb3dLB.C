cfb8ed85684a57d668cbd9554c5feed2071cd011
Pritish Jetley <pjetley2@illinois.edu>
2011-06-23 02:12:46 -0500

    in Orb3dLB, use ckvecs instead of normal arrays

--- Orb3dLB.C
+++ Orb3dLB.C
@@ -131,7 +131,7 @@ void Orb3dLB::work(BaseLB::LDStats* stats, int count)
   int nmig = stats->n_migrateobjs;
 
   CkPrintf("[orb3dlb] %d objects allocating %d bytes for tp\n", numobjs, numobjs*sizeof(TPObject));
-  TPObject *tp = new TPObject[numobjs];
+  tps.resize(numobjs);
 
   stats->makeCommHash();
   CkPrintf("[orb3dlb] ready tp data structure\n");
@@ -141,17 +141,17 @@ void Orb3dLB::work(BaseLB::LDStats* stats, int count)
   for(int i = 0; i < stats->n_objs; i++){
     LDObjHandle &handle = tpCentroids[i].handle;
     int tag = stats->getHash(handle.id,handle.omhandle.id);
-    tp[tag].centroid.x = tpCentroids[i].vec.x;
-    tp[tag].centroid.y = tpCentroids[i].vec.y;
-    tp[tag].centroid.z = tpCentroids[i].vec.z;
-    tp[tag].migratable = stats->objData[tag].migratable;
+    tps[tag].centroid.x = tpCentroids[i].vec.x;
+    tps[tag].centroid.y = tpCentroids[i].vec.y;
+    tps[tag].centroid.z = tpCentroids[i].vec.z;
+    tps[tag].migratable = stats->objData[tag].migratable;
     if(step() == 0){
-      tp[tag].load = tpCentroids[i].myNumParticles;
+      tps[tag].load = tpCentroids[i].myNumParticles;
     }
     else{
-      tp[tag].load = stats->objData[tag].wallTime;
+      tps[tag].load = stats->objData[tag].wallTime;
     }
-    tp[tag].lbindex = tag;
+    tps[tag].lbindex = tag;
     /*
     if(step() == 1){
       CkPrintf("[tpinfo] %f %f %f %f %d %d\n",
@@ -178,7 +178,7 @@ void Orb3dLB::work(BaseLB::LDStats* stats, int count)
   int numnodes = nx*ny*nz; 
 
   CkPrintf("[orb3dlb] %d numnodes allocating %d bytes for nodes\n", numnodes, numnodes*sizeof(Node));
-  Node *nodes = new Node[numnodes];
+  nodes.resize(numnodes);
 
   for(int i = 0; i < stats->count; i++){
     int t;
@@ -195,7 +195,11 @@ void Orb3dLB::work(BaseLB::LDStats* stats, int count)
   }
 
   CkPrintf("[orb3dlb] map\n");
-  map(tp,numobjs,numnodes,nodes,nx,ny,nz,dim);
+  int tpstart = 0;
+  int tpend = numobjs;
+  int nodestart = 0;
+  int nodeend = numnodes;
+  map(tpstart,tpend,nodestart,nodeend,nx,ny,nz,dim);
 
   for(int i = 0; i < numnodes; i++){
     CkPrintf("bb of node %d %f %f %f %f %f %f\n",
@@ -239,27 +243,29 @@ void Orb3dLB::work(BaseLB::LDStats* stats, int count)
   */
 
   //delete[] objload;
-  delete[] tp;
-  delete[] nodes;
 
 }
 
-void Orb3dLB::map(TPObject *tp, int ntp, int nn, Node *nodes, int xs, int ys, int zs, int dim){
+void Orb3dLB::map(int tpstart, int tpend, int nodestart, int nodeend, int xs, int ys, int zs, int dim){
   //CkPrintf("ntp: %d np: %d dim: %d path: 0x%x\n",ntp,np,dim,path);
+  int nn = nodeend-nodestart;
   if(nn == 1){
-    directMap(tp,ntp,nodes);
+    directMap(tpstart,tpend,nodestart,nodeend);
   }
   else{
-    int totalTp = ntp;
+    int totalTp = tpend-tpstart;
     
-    qsort(tp,ntp,sizeof(TPObject),compares[dim]);
-    qsort(nodes,nn,sizeof(Node),pc[dim]);
+    qsort(tps.getVec()+tpstart,totalTp,sizeof(TPObject),compares[dim]);
+    qsort(nodes.getVec()+nodestart,nn,sizeof(Node),pc[dim]);
     // tp and ntp are modified to hold the particulars of
     // the left/dn/near partitions
     // tp2 and totalTp-ntp hold the objects in the 
     // right/up/far partitions
-    TPObject *tp2 = partitionEvenLoad(tp,ntp);
-    Node *nodes2 = halveNodes(nodes,nn);
+    int tpmid;
+    int nodemid;
+    partitionEvenLoad(tpstart, tpend, tpmid);
+    halveNodes(nodestart, nodeend, nodemid);
+
     int d = nextDim(dim,xs,ys,zs); 
     if(d == 0){
       xs >>= 1;
@@ -270,30 +276,28 @@ void Orb3dLB::map(TPObject *tp, int ntp, int nn, Node *nodes, int xs, int ys, in
     else{
       zs >>= 1;
     }
-    map(tp,ntp,nn/2,nodes,xs,ys,zs,d);
-    map(tp2,totalTp-ntp,nn/2,nodes2,xs,ys,zs,d);
+    map(tpstart,tpmid,nodestart,nodemid,xs,ys,zs,d);
+    map(tpmid,tpend,nodemid,nodeend,xs,ys,zs,d);
   }
 }
 
 #define ZERO_THRESHOLD 0.00001
 
-void Orb3dLB::directMap(TPObject *tp, int ntp, Node *nodes){
+void Orb3dLB::directMap(int tpstart, int tpend, int nodestart, int nodeend){
   //CkPrintf("[Orb3dLB] mapping %d objects to Node (%d,%d,%d)\n", ntp, nodes[0].x, nodes[0].y, nodes[0].z);
 
-  float load = 0.0;
-  for(int i = 0; i < ntp; i++){
-    //CkPrintf("obj %d thisindex %d %d %f %f %f %f to node %d %d %d\n", tp[i].lbindex, tp[i].index, tp[i].nparticles, tp[i].load, tp[i].centroid.x, tp[i].centroid.y, tp[i].centroid.z, nodes[0].x, nodes[0].y, nodes[0].z);
-    load += tp[i].load;
-  }
-  //CkPrintf("node %d %d %d total load %f\n", nodes[0].x, nodes[0].y, nodes[0].z, load);
-  
   std::priority_queue<TPObject> pq_obj;
   std::priority_queue<Processor> pq_proc;
 
-  for(int i = 0; i < ntp; i++){
-    pq_obj.push(tp[i]);
+  float load = 0.0;
+  CkAssert(nodestart==(nodeend-1));
+  for(int i = tpstart; i < tpend; i++){
+    //CkPrintf("obj %d thisindex %d %d %f %f %f %f to node %d %d %d\n", tp[i].lbindex, tp[i].index, tp[i].nparticles, tp[i].load, tp[i].centroid.x, tp[i].centroid.y, tp[i].centroid.z, nodes[0].x, nodes[0].y, nodes[0].z);
+    load += tps[i].load;
+    pq_obj.push(tps[i]);
   }
-
+  //CkPrintf("node %d %d %d total load %f\n", nodes[0].x, nodes[0].y, nodes[0].z, load);
+  
   for(int i = 0; i < procsPerNode; i++){
     Processor p;
     p.load = 0.0;
@@ -326,8 +330,8 @@ void Orb3dLB::directMap(TPObject *tp, int ntp, Node *nodes){
       //CkPrintf("proc %d load %f gets obj %d load %f\n", p.t, p.load, tp.lbindex, tp.load);
 
       p.load += tp.load;
-      (*mapping)[tp.lbindex] = nodes[0].procRanks[p.t];
-      nodes[0].box.grow(tp.centroid);
+      (*mapping)[tp.lbindex] = nodes[nodestart].procRanks[p.t];
+      nodes[nodestart].box.grow(tp.centroid);
 
       pq_proc.push(p);
     }
@@ -350,46 +354,49 @@ int Orb3dLB::nextDim(int dim_, int xs, int ys, int zs){
 
 #define LOAD_EQUAL_TOLERANCE 1.02
 
-TPObject *Orb3dLB::partitionEvenLoad(TPObject *tp, int &ntp){
-  float partition1Load = 0.0;
+void Orb3dLB::partitionEvenLoad(int tpstart, int tpend, int &tpmid){
   float totalLoad = 0.0;
-  for(int i = 0; i < ntp; i++){
-    totalLoad += tp[i].load;
+  for(int i = tpstart; i < tpend; i++){
+    totalLoad += tps[i].load;
   }
-  float halfLoad = 0.5*totalLoad;
-  //CkPrintf("partitionEvenLoad total load %f half load %f\n", totalLoad, halfLoad);
-  int split = -1;
+  float lload = 0.0;
+  float rload = totalLoad;
+  float prevDiff = lload-rload;
+  if(prevDiff < 0.0){
+    prevDiff = -prevDiff;
+  }
 
-  for(int i = 0; i < ntp; i++){
-    // if including this element in partition1 brings us closer
-    // to halfLoad, do it
-    //if((partition1Load+tp[i].load-halfLoad) < (halfLoad-partition1Load)){
-    if((partition1Load+tp[i].load <= halfLoad) ||
-       (partition1Load < halfLoad && partition1Load+tp[i].load > halfLoad)){
-      partition1Load += tp[i].load;
-      split++;
+  int consider;
+  for(consider = tpstart; consider < tpend;){
+    float newll = lload + tps[consider].load;
+    float newrl = rload - tps[consider].load;
+
+    float newdiff = newll-newrl;
+    if(newdiff < 0.0){
+      newdiff = -newdiff;
+    }
+
+    //CkPrintf("consider load %f newdiff %f prevdiff %f\n", tp[consider].load, newdiff, prevDiff);
+
+    if(newdiff < prevDiff){
+      consider++;
+      lload = newll;
+      rload = newrl;
+      prevDiff = newdiff;
     }
     else{
       break;
     }
   }
 
-  //float lload = 0.0;
-  //for(int i = 0; i < split+1; i++){
-  //  lload += tp[i].load;
-  //}
-  //float rload = totalLoad-lload;
+  //CkPrintf("partitionEvenLoad lload %f rload %f\n", lload, rload);
 
-  //CkPrintf("partitionEvenLoad partition1Load %f lsplit %f rsplit %f\n", partition1Load, lload, rload);
-
-  ntp = split+1;
-  return (tp+split+1);
+  tpmid = consider;
 }
 
-Node *Orb3dLB::halveNodes(Node *start, int np){
-  Node *ret = start;
-  ret = start+np/2;
-  return ret;
+void Orb3dLB::halveNodes(int nodestart, int nodeend, int &nodemid){
+  int np = nodeend-nodestart;
+  nodemid = nodestart+np/2;
 }
 
 #include "Orb3dLB.def.h"
