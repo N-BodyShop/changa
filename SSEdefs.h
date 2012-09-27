8f12a0ce10dd74de312b680fc7d5ac7421284d38
Tom Quinn <trq@astro.washington.edu>
2012-09-27 10:44:30 -0700

    Distinguish between "COSMO_FLOAT", the type used for particle positions, and "SSE_COSMO_FLOAT", the type used in the SSE force calculations.
    Change-Id: I29e1d33c653db8295d21bde3f8ea58504b318615

--- SSEdefs.h
+++ SSEdefs.h
@@ -4,78 +4,77 @@
 #include "cosmoType.h"
 
 #if  CMK_USE_AVX
-#if !defined(__AVX__)
-#undef CMK_USE_AVX
-#define CMK_USE_AVX            0
-#else
-#warning "using AVX"
-#endif
+	#if !defined(__AVX__)
+		#undef CMK_USE_AVX
+		#define CMK_USE_AVX 0
+	#else
+		#warning "using AVX"
+	#endif
 #endif
 
-#if  CMK_USE_SSE2 && !defined(__SSE2__)
-#undef CMK_USE_SSE2
-#define CMK_USE_SSE2            0
+#if CMK_USE_SSE2 && !defined(__SSE2__)
+	#undef CMK_USE_SSE2
+	#define CMK_USE_SSE2 0
 #endif
 
 #if CMK_USE_AVX || CMK_USE_SSE2
-#define CMK_SSE                 1
+	#define CMK_SSE 1
 #endif
 
 #if CMK_USE_AVX
-
-#ifndef COSMO_FLOAT
-
-#include "SSE-Double.h"
-#define SSE_VECTOR_WIDTH 4
-#define FORCE_INPUT_LIST_PAD 3
-typedef SSEDouble SSEcosmoType; 
-#define SSELoad(where, arr, idx, field) where(arr[idx]field, arr[idx+1]field, arr[idx+2]field, arr[idx+3]field)
-#define SSEStore(what, arr, idx, field) { \
-  double p[4];  \
-  storeu(p, what); \
-  arr[idx]field = p[0]; \
-  arr[idx+1]field = p[1]; \
-  arr[idx+2]field = p[2]; \
-  arr[idx+3]field = p[3]; \
-} 
-enum { cosmoMask=0xf };
-
-#endif /* COSMO_FLOAT */
-
+	#ifndef COSMO_FLOAT
+		#include "SSE-Double.h"
+		#define SSE_VECTOR_WIDTH 4
+		#define FORCE_INPUT_LIST_PAD 3
+		typedef SSEDouble SSEcosmoType;
+		#define SSELoad(where, arr, idx, field) where(arr[idx]field, arr[idx+1]field, arr[idx+2]field, arr[idx+3]field)
+		#define SSEStore(what, arr, idx, field) { \
+		  double p[4];  \
+		  storeu(p, what); \
+		  arr[idx]field = p[0]; \
+		  arr[idx+1]field = p[1]; \
+		  arr[idx+2]field = p[2]; \
+		  arr[idx+3]field = p[3]; \
+		}
+		enum {cosmoMask=0xf};
+	#endif
 #elif CMK_USE_SSE2
+	#ifdef COSMO_FLOAT
+		#define SSE_COSMO_FLOAT
+		#if defined(__SSE2__)
+			#include "SSE-Float.h"
+			#define SSE_VECTOR_WIDTH 4
+			#define FORCE_INPUT_LIST_PAD 3
+			typedef SSEFloat SSEcosmoType;
+			#define SSELoad(where, arr, idx, field) where(arr[idx]field, arr[idx+1]field, arr[idx+2]field, arr[idx+3]field)
+			#define SSEStore(what, arr, idx, field) { \
+			  float p[4]; \
+			  storeu(p, what); \
+			  arr[idx]field = p[0]; \
+			  arr[idx+1]field = p[1]; \
+			  arr[idx+2]field = p[2]; \
+			  arr[idx+3]field = p[3]; \
+			}
+			enum {cosmoMask=0xf};
+                #else
+                      #error("SSE not available");
+                #endif
+        #else
+		#if defined(__SSE2__) && !defined(SSE_COSMO_FLOAT)
+			#include "SSE-Double.h"
+			#define SSE_VECTOR_WIDTH 2
+			#define FORCE_INPUT_LIST_PAD 1
+			typedef SSEDouble SSEcosmoType;
+			#define SSELoad(where, arr, idx, field) where(arr[idx]field, arr[idx+1]field)
+			#define SSEStore(what, arr, idx, field) { \
+			  storel(&arr[idx]field, what); \
+			  storeh(&arr[idx+1]field, what); \
+			}
+			enum {cosmoMask=0x3};
+                #else
+                      #error("SSE not available");
+		#endif
+	#endif
+#endif
 
-#ifdef COSMO_FLOAT
-
-#include "SSE-Float.h"
-#define SSE_VECTOR_WIDTH 4
-#define FORCE_INPUT_LIST_PAD 3
-typedef SSEFloat SSEcosmoType; 
-#define SSELoad(where, arr, idx, field) where(arr[idx]field, arr[idx+1]field, arr[idx+2]field, arr[idx+3]field)
-#define SSEStore(what, arr, idx, field) { \
-  float p[4]; \
-  storeu(p, what); \
-  arr[idx]field = p[0]; \
-  arr[idx+1]field = p[1]; \
-  arr[idx+2]field = p[2]; \
-  arr[idx+3]field = p[3]; \
-}
- 
-enum { cosmoMask=0xf };
-#elif defined(__SSE2__) && !defined(COSMO_FLOAT)
-
-#include "SSE-Double.h"
-#define SSE_VECTOR_WIDTH 2
-#define FORCE_INPUT_LIST_PAD 1
-typedef SSEDouble SSEcosmoType; 
-#define SSELoad(where, arr, idx, field) where(arr[idx]field, arr[idx+1]field)
-#define SSEStore(what, arr, idx, field) { \
-  storel(&arr[idx]field, what); \
-  storeh(&arr[idx+1]field, what); \
-} 
-enum { cosmoMask=0x3 };
-
-#endif   /* end of COSMO_FLOAT under CMK_USE_SSE2 */
-
-#endif    /*  CMK_USE_AVX  */
-
-#endif   /* end of __SSEDEFS_H__ */
+#endif
