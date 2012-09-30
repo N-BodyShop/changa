fbbceabec5dd35aaa103d5449379b878d59d4d08
Thomas Quinn <trq@astro.washington.edu>
2012-09-29 17:55:31 -0800

    Don't make COSMO_FLOAT the default (for now).

--- cosmoType.h
+++ cosmoType.h
@@ -1,7 +1,8 @@
 #ifndef __COSMOTYPE_H__
 #define __COSMOTYPE_H__
 
-#define COSMO_FLOAT
+/* #define COSMO_FLOAT */
+
 #ifdef COSMO_FLOAT
 #define CONVERT_TO_COSMO_TYPE (float)
 typedef float cosmoType;
