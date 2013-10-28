d2938e56516b04f4e2a0571fe91c61eee5e8fb1c
Alexander Fry <bastidas@noreaster.astro.washington.edu>
2013-10-28 11:37:41 -0700

    moving along

--- SIDM.C
+++ SIDM.C
@@ -8,8 +8,15 @@
 #include "ParallelGravity.h"
 #include "smooth.h"
 #include "SIDM.h"
+//#include "physconst.h"
+
 
 //see dendvdsmoothparams
+
+
+//void SIDM::AddParams(PRM prm, Parameters &params) 
+//{
+//}
 
 void Main::doSIDM(double dTime,double dDelta, int activeRung)
 {
@@ -43,7 +50,7 @@ void SIDMSmoothParams::initTreeParticle(GravityParticle *p)
 
 void SIDMSmoothParams::initSmoothCache(GravityParticle *p)
 {
-//        p->nSIDMCollisionCount = 0;
+    //p->iNSIDMInteractions = 0;
     p->curlv()[1] = 0.0;
     p->curlv()[0] = 0.0; 
     p->curlv()[2] = 0.0; 
@@ -54,7 +61,7 @@ void SIDMSmoothParams::combSmoothCache(GravityParticle *p1, ExternalSmoothPartic
     //p1->curlv()[0] = p2->curlv[0];
     //p1->curlv()[1] = p2->curlv[1];
     //p1->curlv()[2] = p2->curlv[2];
-    //p1->nSIDMCollisionCount += p2->nSIDMCollisionCount;
+    //p1->iNSIDMInteractions += p1-> iNSIDMInteractions;
     //p1->iType |= p2->iType;
     }
 
@@ -71,12 +78,17 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
     double norm,vxcm,vycm,vzcm, m1,m2;
     //double ftempe; //only used in print diagnostics
     double uvar,vvar;
+
+    //dSIDMSigma=dSIDMSigma*param.dMsolUnit*MSOLG/(param.dKpcUnit*KPCCM*param.dKpcUnit*KPCCM);
+
     
     //    vFac = 1./(a*a); /* converts v to xdot */
     aDot=a*H;
     ih2 = invH2(p); //grabs smoothing length, 1/h^2
     fNorm = M_1_PI*ih2*sqrt(ih2); //
     fDensity = 0.0;
+
+    //CkPrintf("<SIDM> dSIDMSigma: %d,  dDelta: %d \n",dSIDMSigma,dDelta);
 
     for (i=0;i<nSmooth;++i)
         {
@@ -91,8 +103,8 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
     for (i=0;i<nSmooth;++i)
         {
         q = nnList[i].p;
-        //ran=rand()/((double)RAND_MAX); /* generate random number between 0 and 1 */
-        ran=rand()/RAND_MAX; /* generate random number between 0 and 1 */
+        ran=rand()/((double)RAND_MAX); /* generate random number between 0 and 1 */
+        //ran=rand()/RAND_MAX; /* generate random number between 0 and 1 */
 
         dvx = (-p->velocity.x + q->velocity.x)/a - aDot*nnList[i].dx.x; //could reduce these to one statment using vector arithmetic
         dvy = (-p->velocity.y + q->velocity.y)/a - aDot*nnList[i].dx.y; //v or vpred?
@@ -103,6 +115,8 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
  *                 mass sim unit = real unit in g * conversion from g to Msol / dMsolUnit
  *                                 distance sim unit = real unit in cm * conversion from cm to kpc / dKpcUnit */              
         if (probability > ran) {
+            p->iNSIDMInteractions += 1;
+            //q->iNSIDMInteractions += 1;
             //p->nSIDMCollisionCount+=1;
             //q->nSIDMCollisionCount+=1;
             m1=p->mass; //is this physical mass?
@@ -116,13 +130,23 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
             vycm=-pycm/m1;
             vzcm=-pzcm/m1;
 
-            ecm0=sqrt(pxcm*pxcm+pycm*pycm+pzcm*pzcm);//for printing out ecm0 is usefull this way
+            ecm0=sqrt(pxcm*pxcm+pycm*pycm+pzcm*pzcm);
+            if (ran < 0.00000000000001) {
+                CkPrintf("<SIDM> i: %i,  dvcosmo: %g \n",i,dvcosmo);
+                CkPrintf("<SIDM> ran: %g,  probability: %g \n",ran, probability);
+                CkPrintf("<SIDM> dSIDMSigma: %g,  dDelta: %g \n",dSIDMSigma,dDelta);
+                CkPrintf("<SIDM> mass p: %g,  mass q: %g \n",p->mass,q->mass);
+                CkPrintf("<SIDM> momentum initial: %g,  density: %g \n",ecm0, fPhyDensity);
+                CkPrintf("<SIDM> px: %g,  py: %g, pz: %g \n",pxcm,pycm,pzcm);
+                CkPrintf("<SIDM> ninteractions: %i \n",p->iNSIDMInteractions);
+                }
+
             norm=666;
-                        while (norm>1.0){ //unit sphere point picking (Marsaglia 1972)
-                            uvar=2.0*(rand()/(double)RAND_MAX)-1.0;  //#random number on [-1,1]
-                            vvar=2.0*(rand()/(double)RAND_MAX)-1.0;
-                            norm=(uvar*uvar+vvar*vvar);
-                            }
+            while (norm>1.0){ //unit sphere point picking (Marsaglia 1972)
+                uvar=2.0*(rand()/(double)RAND_MAX)-1.0;  //#random number on [-1,1]
+                vvar=2.0*(rand()/(double)RAND_MAX)-1.0;
+                norm=(uvar*uvar+vvar*vvar);
+                }
             ux=2.0*uvar*sqrt(1-uvar*uvar-vvar*vvar);
             uy=2.0*vvar*sqrt(1-uvar*uvar-vvar*vvar);
             uz=1.0-2.0*(uvar*uvar+vvar*vvar);
@@ -138,6 +162,24 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
             q->velocity.x+=a*(-pxcm/m2+vxcm-dvx);
             q->velocity.y+=a*(-pycm/m2+vycm-dvy);
             q->velocity.z+=a*(-pzcm/m2+vzcm-dvz);
+
+
+
+            if (ran < 0.00000000000001) {
+                dvx = (-p->velocity.x + q->velocity.x)/a - aDot*nnList[i].dx.x; 
+                dvy = (-p->velocity.y + q->velocity.y)/a - aDot*nnList[i].dx.y;
+                dvz = (-p->velocity.z + q->velocity.z)/a - aDot*nnList[i].dx.z;
+                dvcosmo = sqrt(dvx*dvx + dvy*dvy + dvz*dvz);
+                pxcm = -mu*dvx;
+                pycm = -mu*dvy;
+                pzcm = -mu*dvz;
+                ecm0=sqrt(pxcm*pxcm+pycm*pycm+pzcm*pzcm);
+                CkPrintf("<SIDM> i: %i,  dvcosmo: %g \n",i,dvcosmo);
+                CkPrintf("<SIDM> momentum final: %g,  density: %g \n",ecm0, fPhyDensity);
+                CkPrintf("<SIDM> px: %g,  py: %g, pz: %g \n",pxcm,pycm,pzcm);
+                }
+         
+
 
             //q->curlv[0]+=aFac*(-pxcm/m2+vxcm-dvx); //this accumulation is for sending q vel update back to home node
             //q->curlv[1]+=aFac*(-pycm/m2+vycm-dvy);
