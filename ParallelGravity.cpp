f62ce797ff1b132cebcad05aa4ad71d13e8cd126
YanhuaSun <sun51@illinois.edu>
2014-02-17 10:33:38 -0600

    add auto tune

--- ParallelGravity.cpp
+++ ParallelGravity.cpp
@@ -1359,6 +1359,7 @@ void Main::advanceBigStep(int iStep) {
 
   while (currentStep < MAXSUBSTEPS) {
 
+      autoPerfGlobalNextStepResume(CkCallbackResumeThread());
     if(!param.bStaticTest) {
       CkAssert(param.dDelta != 0.0);
       // Find new rung for active particles
