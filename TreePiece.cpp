584dba08e5fe5b1447cf8f1531a00e32070563df
Harshitha <gplkrsh2@illinois.edu>
2014-05-02 14:17:23 -0500

    Call TPWorkDone before finishWalk
    Change-Id: I31e39a1c7915976dd4bdd49788a65c2a02062859

--- TreePiece.cpp
+++ TreePiece.cpp
@@ -4393,6 +4393,7 @@ void TreePiece::startGravity(int am, // the active mask for multistepping
     for (int i=0; i< numChunks; ++i) {
       cacheGravPart.ckLocalBranch()->finishedChunk(i, 0);
     }
+    nodeLBMgrProxy.ckLocalBranch()->finishedTPWork();
     CkCallback cbf = CkCallback(CkIndex_TreePiece::finishWalk(), pieces);
     gravityProxy[thisIndex].ckLocal()->contribute(cbf);
     return;
@@ -5765,6 +5766,7 @@ void TreePiece::markWalkDone() {
 #ifdef CHECK_WALK_COMPLETIONS
         CkPrintf("[%d] inside markWalkDone, completedActiveWalks: %d, activeWalks: %d, contrib finishWalk\n", thisIndex, completedActiveWalks, activeWalks.size());
 #endif
+    nodeLBMgrProxy.ckLocalBranch()->finishedTPWork();
 	CkCallback cb = CkCallback(CkIndex_TreePiece::finishWalk(), pieces);
 	gravityProxy[thisIndex].ckLocal()->contribute(cb);
 	}
@@ -5901,7 +5903,6 @@ void TreePiece::finishWalk()
   
 #endif
 
-  nodeLBMgrProxy.ckLocalBranch()->finishedTPWork();
   gravityProxy[thisIndex].ckLocal()->contribute(cbGravity);
 }
 
