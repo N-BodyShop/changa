f22cbf66cc05e0c4a95582971f035ba0f395d835
Harshitha <gplkrsh2@illinois.edu>
2014-05-09 20:33:10 -0500

    Intra node load balancing using CkLoop
    Change-Id: Id1b94c1aef9257161f65ccddd27b1c756dc9d323

---
+++ IntraNodeLBManager.h
@@ -0,0 +1,28 @@
+#ifndef INTRA_NODE_LB_MANAGER_H
+#define INTRA_NODE_LB_MANAGER_H
+
+#include <vector>
+
+using std::vector;
+
+class IntraNodeLBManager : public CBase_IntraNodeLBManager {
+
+ public:
+  IntraNodeLBManager(int dummy, CkGroupID gid);
+
+  IntraNodeLBManager(CkMigrateMessage *m);
+
+  void pup(PUP::er &p);
+ 
+  void registerTP();
+  void finishedTPWork();
+
+  vector<int> getOtherIdlePes();
+
+ private:
+  int total_tps_;
+  int tps_done_;
+  int num_loc_mgr_;
+  CkGroupID *loc_mgr_;
+};
+#endif
---
+++ MultistepNodeLB_notopo.h
@@ -0,0 +1,97 @@
+/*****************************************************************************
+ * $Source$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *****************************************************************************/
+
+/**
+ * \addtogroup CkLdb
+*/
+/*@{*/
+
+#ifndef _MULTISTEPNODELB_NOTOPO_H_
+#define _MULTISTEPNODELB_NOTOPO_H_
+
+#define MCLBMS          // multistepping enabled
+#define MCLB_ORBSMOOTH  // orbsmooth for large steps
+#define MCLB_RR         // round robin otherwise
+
+#include "Orb3dLBCommon.h"
+
+#include "MultistepNodeLB_notopo.decl.h"
+
+void CreateMultistepLB_notopo();
+BaseLB * AllocateMultistepNodeLB_notopo();
+
+
+/// @brief Multistep load balancer where no processor topology
+/// information is used. This first performs orb partition at the node level and
+/// assigns the partitioned TreePieces to the PEs belonging to the node. Finally
+/// after this assignment, a refinement is done.
+class MultistepNodeLB_notopo : public CentralLB, public Orb3dCommon {
+private:
+
+  int prevMaxPredPe;
+
+  void init();
+  bool QueryBalanceNow(int step);
+
+public:
+  MultistepNodeLB_notopo(const CkLBOptions &);
+  MultistepNodeLB_notopo(CkMigrateMessage *m):CentralLB(m) { 
+    init();
+  }
+    
+  void work(BaseLB::LDStats* stats);
+  void balanceTPs(BaseLB::LDStats* stats);
+  void balanceTPsNode(BaseLB::LDStats* stats);
+
+public:/* <- Sun CC demands Partition be public for ComputeLoad to access it. */
+
+  class Partition {
+  public:
+    int refno;
+    double load;			// total load in this set
+    int origin[3];			// box coordinates
+    int corner[3];
+    int  count;				// number of objects in this partition
+    int node, mapped;
+    CkVec<int>   bkpes;			// background processors
+  public:
+    Partition(): refno(0), load(0.0), node(-1), mapped(0) {};
+  };
+
+private:  
+  struct ComputeLoad {
+    int id;
+    int v[3];
+    double load;
+    int  refno;
+    double  tv;
+    Partition * partition;
+  };
+  
+  
+  struct VecArray {
+    int v;
+    int id;
+  };
+  
+  enum {XDIR=0, YDIR, ZDIR};
+  
+public:
+  
+//**************************************
+// ORB3DLB functions
+//**************************************
+//
+  void work2(BaseLB::LDStats* stats, int count);
+  void greedy(BaseLB::LDStats* stats, int count);
+ 
+  void pup(PUP::er &p);
+};
+
+#endif /* _MultistepNodeLB_notopo */
+
+/*@}*/
