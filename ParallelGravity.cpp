4185a5162d656d0bdb87a2599198e29c516d6470
Harshitha <gplkrsh2@illinois.edu>
2014-05-09 20:33:10 -0500

    Intra node load balancing using CkLoop
    commit 6a10d6aa7d83ecb6323aa033ef82d69fec0392ff
    Author: Harshitha <gplkrsh2@illinois.edu>
    Date:   Fri May 2 14:17:23 2014 -0500
    
        Call TPWorkDone before finishWalk
    
        Change-Id: Ib118d156cb01f0b93a55176a98d8164c26933290
    
    commit 3f599bb98488be2a1c73d28e799ba196a927d586
    Author: Harshitha <gplkrsh2@illinois.edu>
    Date:   Mon Apr 28 20:49:58 2014 -0500
    
        Intra node load balancing using CkLoop
    
    commit 760de0f3b7d294712e11cdec74b30311a7bf540e
    Author: Harshitha <gplkrsh2@illinois.edu>
    Date:   Mon Apr 28 19:48:42 2014 -0500
    
        Intra node load balancing using CkLoop
    
        Change-Id: Ib21eb0a0d2a37e765ee3064ce420b1b310621185
    
    Change-Id: Id1b94c1aef9257161f65ccddd27b1c756dc9d323

--- ParallelGravity.cpp
+++ ParallelGravity.cpp
@@ -29,10 +29,12 @@
 // #include <fenv.h>
 
 #include "BaseLB.h"
+#include "CkLoopAPI.h"
 
 #include "Sorter.h"
 #include "ParallelGravity.h"
 #include "DataManager.h"
+#include "IntraNodeLBManager.h"
 #include "TipsyFile.h"
 #include "param.h"
 #include "smooth.h"
@@ -67,6 +69,7 @@ CProxy_CkCacheManager<KeyType> cacheGravPart;
 CProxy_CkCacheManager<KeyType> cacheSmoothPart;
 CProxy_CkCacheManager<KeyType> cacheNode;
 CProxy_DataManager dMProxy;
+CProxy_IntraNodeLBManager nodeLBMgrProxy;
 
 CProxy_DumpFrameData dfDataProxy;
 CProxy_PETreeMerger peTreeMergerProxy;
@@ -93,6 +96,7 @@ int _numChunks;
 int _randChunks;
 unsigned int bucketSize;
 int lbcomm_cutoff_msgs;
+int bUseCkLoopPar;
 
 //jetley
 int localNodesPerReq;
@@ -188,6 +192,8 @@ Main::Main(CkArgMsg* m) {
 	bIsRestarting = 0;
 	bChkFirst = 1;
 	dSimStartTime = CkWallTimer();
+
+  int threadNum = CkMyNodeSize();
 
 	// Floating point exceptions.
 	// feenableexcept(FE_OVERFLOW | FE_DIVBYZERO | FE_INVALID);
@@ -579,7 +585,10 @@ Main::Main(CkArgMsg* m) {
 	prmAddParam(prm, "bLiveViz", paramInt,&param.bLiveViz, sizeof(int),
 		    "liveviz", "enable real-time simulation render support (disabled)");
 
-	
+	param.bUseCkLoopPar = 0;
+	prmAddParam(prm, "bUseCkLoopPar", paramInt,&param.bUseCkLoopPar, sizeof(int),
+		    "useckloop", "enable CkLoop to parallelize within node");
+
 	param.bStaticTest = 0;
 	prmAddParam(prm, "bStaticTest", paramBool, &param.bStaticTest,
 		    sizeof(int),"st", "Static test of performance");
@@ -779,6 +788,12 @@ Main::Main(CkArgMsg* m) {
 	dMaxBalance = param.dMaxBalance;
 	_cacheLineDepth = param.cacheLineDepth;
 	nIOProcessor = param.nIOProcessor;
+#if CMK_SMP
+  bUseCkLoopPar = param.bUseCkLoopPar;
+#else
+  bUseCkLoopPar = 0;
+#endif
+
 	if(prmSpecified(prm, "bCannonical")) {
 	    ckerr << "WARNING: ";
 	    ckerr << "bCannonical parameter ignored; integration is always cannonical"
@@ -1097,6 +1112,7 @@ Main::Main(CkArgMsg* m) {
 
         peTreeMergerProxy = CProxy_PETreeMerger::ckNew();
         dfDataProxy = CProxy_DumpFrameData::ckNew();
+//nodeLBMgrProxy = nlb; 
 	
 	// create CacheManagers
 	// Gravity particles
@@ -1110,11 +1126,14 @@ Main::Main(CkArgMsg* m) {
 	CProxy_DataManager dataManager = CProxy_DataManager::ckNew(pieces);
 	dataManagerID = dataManager;
         dMProxy = dataManager;
+  nodeLBMgrProxy = CProxy_IntraNodeLBManager::ckNew(1,pieces.ckLocMgr()->getGroupID());
 
 	streamingProxy = pieces;
 
 	//create the Sorter
 	sorter = CProxy_Sorter::ckNew(0);
+
+  CkLoop_Init(threadNum);
 
 	if(verbosity)
 	  ckerr << "Created " << numTreePieces << " pieces of tree" << endl;
@@ -2048,6 +2067,8 @@ Main::restart()
 	prmAddParam(prm,"dMaxBalance",paramDouble,&param.dMaxBalance,
 		    sizeof(double), "maxbal",
 		    "Maximum piece ratio for load balancing");
+	prmAddParam(prm, "bUseCkLoopPar", paramInt,&param.bUseCkLoopPar, sizeof(int),
+		    "useckloop", "enable CkLoop to parallelize within node");
 
         int processSimfile = 0; 
 	if(!prmArgProc(prm,CmiGetArgc(args->argv),args->argv,processSimfile)) {
