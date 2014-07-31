d8657e62f90b8728362f7ad23ba3bf0e489d5521
Harshitha <gplkrsh2@illinois.edu>
2014-07-31 07:39:43 -0500

    Pritish's changes to ChaNGa to use SMP cache
    Change-Id: I53d9512650547af8fa4ed7006fc65bd427946461

--- Orb3dLB_notopo.C
+++ Orb3dLB_notopo.C
@@ -130,7 +130,9 @@ void Orb3dLB_notopo::work(BaseLB::LDStats* stats)
 	FILE *fp = fopen(achFileName, "w");
 	CkAssert(fp != NULL);
 	fprintf(fp, "%d %d 0\n", nrecvd, nrecvd);
-	for(int i = 0; i < nrecvd; i++) {
+	for(int i = 0; i < numobjs; i++) {
+	    if(stats->objData[i].migratable == false)
+		continue;
 	    CkAssert(tps[i].lbindex < stats->n_objs);
 	    CkAssert(tps[i].lbindex >= 0);
 	    fprintf(fp, "%g %g %g %g 0.0 0.0 0.0 %d 0.0\n",
