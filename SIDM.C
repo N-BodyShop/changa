91b5fadc1dc71d9c4cc0459443d6fe2452716046
Alex Fry <bastidas@uw.edu>
2014-08-14 14:55:59 -0700

    check on large probability inserted

--- SIDM.C
+++ SIDM.C
@@ -64,8 +64,6 @@ void SIDMSmoothParams::combSmoothCache(GravityParticle *p1, ExternalSmoothPartic
     //p1->iType |= p2->iType;
     }
 
-
-//void SIDMSmoothParams::doSIDM(GravityParticle *p, int nSmooth, pqSmoothNode *nnList)
 void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *nnList)
     {
     GravityParticle *q;
@@ -76,11 +74,6 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
     double mu, pxcm,pycm,pzcm,pcm0,ux,uy,uz;
     double norm,vxcm,vycm,vzcm, m1,m2;
     double uvar,vvar;
-    //double e1,e2,etot_init,etot_final;
-    double ran2,pvxcm,pvycm,pvzcm; //delete these, testing
-    double qvxcm,qvycm,qvzcm; //delete thes
-
-    //dSIDMSigma=dSIDMSigma*param.dMsolUnit*MSOLG/(param.dKpcUnit*KPCCM*param.dKpcUnit*KPCCM);
 
     aDot=a*H;
     ih2 = invH2(p); //grabs smoothing length, 1/h^2
@@ -94,12 +87,13 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
         q = nnList[i].p;
         fDensity += rs*q->mass;
         }
-        fPhyDensity=fNorm*fDensity/(a*a*a); /*in cosmolgical units */
+
+    fPhyDensity=fNorm*fDensity/(a*a*a); /*in cosmolgical units */
+
     for (i=0;i<nSmooth;++i)
         {
         q = nnList[i].p;
         ran=rand()/((double)RAND_MAX); /* generate random number between 0 and 1 */
-        ran2=rand()/((double)RAND_MAX); /* generate random number between 0 and 1, for random diagnostic print outs */
 
         dvx = (-p->velocity.x + q->velocity.x)/a - aDot*nnList[i].dx.x; //could reduce these to one statment using vector arithmetic
         dvy = (-p->velocity.y + q->velocity.y)/a - aDot*nnList[i].dx.y; //v or vpred?
@@ -109,27 +103,14 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
 
         probability=dSIDMSigma*dvcosmo*dDelta*fNorm*KERNEL( r2 )*(p->mass)/(a*a*a*2.0);
 
-
-        /*print out some diagnostics */
-
-    /*    if (p->iOrder%20000==0 && i==0) {
-                CkPrintf("<SIDM a diagnostic>, %i, %i, %g, %g, %g, %g, %g, %g, %g, %g \n",p->iOrder, q->iOrder, fPhyDensity, dvcosmo, probability, nnList[i].dx.x,nnList[i].dx.y, nnList[i].dx.z, a, aDot);
-                CkPrintf("<SIDM b diagnostic>, %i, %i, %g, %g, %g \n",p->iOrder, q->iOrder, fPhyDensity, dDelta, fNorm*KERNEL( r2 )*(p->mass));
-                CkPrintf("<SIDM c diagnostic>, %g, %g, %g \n",r2,(-p->velocity.x + q->velocity.x)/a , aDot*nnList[i].dx.x );
-	}*/
-
+        if (probability > .01) {
+           CkPrintf("Warning! The probability is rather lage: %g \n",probability);
+           CkPrintf("dDelta: %g,  dvcosmo: %g \n",dDelta,dvcosmo);
+           }
 
         if (probability > ran) {
             p->iNSIDMInteractions += 1;
 
-            if (q->iOrder== p->iOrder)//turn this into asssert
-                {
-                CkPrintf("<bad meta-self-interaction occured on equal iorder> i: %i,  probability: %g \n",i,probability);
-                CkPrintf("<bad meta-self-interaction occured> i: %i,  dvcosmo: %g \n",i,dvcosmo);
-                CkPrintf("<bad meta-self-interaction occured> p->iOrder: %i,  q->iOrder: %i \n", p->iOrder,  q->iOrder);
-                CkPrintf("<bad meta-self-interaction occured> p->iNSIDMInteractions : %i, q->iNSIDMInteractions: %i \n", p->iNSIDMInteractions , q->iNSIDMInteractions );
-                //continue;
-                }
             m1=p->mass; 
             m2=q->mass;
             mu=m1*m2/(m1+m2);
@@ -142,25 +123,6 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
             vzcm=-pzcm/m1;
 
             pcm0=sqrt(pxcm*pxcm+pycm*pycm+pzcm*pzcm);
-  	    //in non-cosmo runs check e conservation directly
-           //     e1=0.5*m1*((p->velocity.x)*(p->velocity.x) + (p->velocity.y)*(p->velocity.y) + (p->velocity.z)*(p->velocity.z));
-           //     e2=0.5*m2*((q->velocity.x)*(q->velocity.x) + (q->velocity.y)*(q->velocity.y) + (q->velocity.z)*(q->velocity.z));
-           //     etot_init=e1+e2;
-           /* if (ran2 < 0.000001) {
-                CkPrintf("<SIDM> i: %i,  dvcosmo: %g \n",i,dvcosmo);
-                CkPrintf("<SIDM> i: %i,  fPhyDensity: %g \n",i,fPhyDensity);
-                CkPrintf("<SIDM> kernelr, r2: %g, %g \n",fNorm*KERNEL( r2 )*(p->mass), r2);
-                CkPrintf("<SIDM> dt: %g \n",dDelta);
-                CkPrintf("<SIDM> vx: %g, vy: %g, vz: %g \n", p->velocity.x,p->velocity.y,p->velocity.z );
-                CkPrintf("<SIDM> dvx: %g, dvy: %g, dvz: %g \n", dvx,dvy,dvz );
-                CkPrintf("<SIDM> ran: %g,  probability: %g \n",ran, probability);
-                CkPrintf("<SIDM> dSIDMSigma: %g,  dDelta: %g \n",dSIDMSigma,dDelta);
-                CkPrintf("<SIDM> mass p: %g,  mass q: %g \n",p->mass,q->mass);
-                CkPrintf("<SIDM> momentum initial: %g,  density: %g \n",pcm0, fPhyDensity);
-                CkPrintf("<SIDM> px: %g,  py: %g, pz: %g \n",pxcm,pycm,pzcm);
-                CkPrintf("<SIDM> ninteractions: %i \n",p->iNSIDMInteractions);
-                CkPrintf("<SIDM> a: %g, adot: %g  \n",a, aDot);
-                }*/
          
             norm=666;
             while (norm>1.0){ //unit sphere point picking (Marsaglia 1972)
@@ -176,7 +138,7 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
             pycm=pcm0*uy;
             pzcm=pcm0*uz;
 
-            p->velocity.x+=a*(pxcm/m1); //equivalent to vcm1finalx
+            p->velocity.x+=a*(pxcm/m1); //equivalent to vcm1_final_x
             p->velocity.y+=a*(pycm/m1);
             p->velocity.z+=a*(pzcm/m1);
             q->velocity.x+=a*(-pxcm/m2);
@@ -190,27 +152,6 @@ void SIDMSmoothParams::fcnSmooth(GravityParticle *p, int nSmooth, pqSmoothNode *
             q->velocity.y+=a*(vycm-dvy);
             q->velocity.z+=a*(vzcm-dvz);
 
-
-            //these are diagnostic print outs
-            //e1=0.5*m1*((p->velocity.x)*(p->velocity.x) + (p->velocity.y)*(p->velocity.y) + (p->velocity.z)*(p->velocity.z));
-            //e2=0.5*m2*((q->velocity.x)*(q->velocity.x) + (q->velocity.y)*(q->velocity.y) + (q->velocity.z)*(q->velocity.z));
-            //etot_final=e1+e2;
-            /*if (ran2 < 0.000001) {
-                dvx = (-p->velocity.x + q->velocity.x)/a - aDot*nnList[i].dx.x; 
-                dvy = (-p->velocity.y + q->velocity.y)/a - aDot*nnList[i].dx.y;
-                dvz = (-p->velocity.z + q->velocity.z)/a - aDot*nnList[i].dx.z;
-                dvcosmo = sqrt(dvx*dvx + dvy*dvy + dvz*dvz);
-                pxcm = -mu*dvx;
-                pycm = -mu*dvy;
-                pzcm = -mu*dvz;
-                pcm0=sqrt(pxcm*pxcm+pycm*pycm+pzcm*pzcm);
-                CkPrintf("<SIDM> i: %i,  dvcosmo: %g \n",i,dvcosmo);
-                CkPrintf("<SIDM> ran: %g,  probability: %g \n",ran, probability);
-                CkPrintf("<SIDM> uvector: %g \n", sqrt(ux*ux + uy*uy + uz*uz) );
-                CkPrintf("<SIDM> momentum final: %g,  density: %g \n",pcm0, fPhyDensity);
-                CkPrintf("<SIDM> px: %g,  py: %g, pz: %g \n",pxcm,pycm,pzcm);
-                }*/
-         
             }
         }    
     }
