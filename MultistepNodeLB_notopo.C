8dfaf509de85637f689a696e8d608d1d173068ab
Harshitha <gplkrsh2@illinois.edu>
2015-03-02 15:01:30 -0600

    In the load balancers check whether the object is migratable before accessing its obj userdata
    Change-Id: I6f7773757bc5f4a04e216620d3e4807e0729c9fa

--- MultistepNodeLB_notopo.C
+++ MultistepNodeLB_notopo.C
@@ -53,6 +53,7 @@ void MultistepNodeLB_notopo::work(BaseLB::LDStats* stats)
       CkAssert(fp != NULL);
       fprintf(fp, "%d %d 0\n", stats->n_objs, stats->n_objs);
       for(int i = 0; i < stats->n_objs; i++) {
+        if(!stats->objData[i].migratable) continue;
 
       LDObjData &odata = stats->objData[i];
       TaggedVector3D* udata = (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
@@ -79,6 +80,8 @@ void MultistepNodeLB_notopo::work(BaseLB::LDStats* stats)
   }
   
   for(int i = 0; i < stats->n_objs; i++){
+    if(!stats->objData[i].migratable) continue;
+
     LDObjData &odata = stats->objData[i];
     TaggedVector3D* udata = (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
 
@@ -104,6 +107,8 @@ void MultistepNodeLB_notopo::work(BaseLB::LDStats* stats)
   if(numInactiveObjects < 1.0*numActiveObjects) {
     // insignificant number of inactive objects; migrate them anyway
     for(int i = 0; i < stats->n_objs; i++){
+      if(!stats->objData[i].migratable) continue;
+
       LDObjData &odata = stats->objData[i];
       TaggedVector3D* udata = 
         (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
@@ -212,6 +217,8 @@ void MultistepNodeLB_notopo::work2(BaseLB::LDStats *stats, int count){
       CkAssert(fp != NULL);
       fprintf(fp, "%d %d 0\n", numobjs, numobjs);
       for(int i = 0; i < numobjs; i++) {
+        if(!stats->objData[i].migratable) continue;
+
     LDObjData &odata = stats->objData[i];
     TaggedVector3D* udata = 
       (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
