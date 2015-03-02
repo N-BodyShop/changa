ee030cfc631e37f567f5e81d9cca9692a19f9f1a
Harshitha <gplkrsh2@illinois.edu>
2015-03-02 15:01:30 -0600

    In the load balancers check whether the object is migratable before accessing its obj userdata
    Change-Id: I6f7773757bc5f4a04e216620d3e4807e0729c9fa

--- MultistepLB_notopo.C
+++ MultistepLB_notopo.C
@@ -69,8 +69,17 @@ void MultistepLB_notopo::work(BaseLB::LDStats* stats)
       sprintf(achFileName, "lb_a.%d.sim", step()-1);
       FILE *fp = fopen(achFileName, "w");
       CkAssert(fp != NULL);
-      fprintf(fp, "%d %d 0\n", stats->n_objs, stats->n_objs);
+
+      int num_migratables = stats->n_objs;
       for(int i = 0; i < stats->n_objs; i++) {
+        if (!stats->objData[i].migratable) {
+          num_migratables--;
+        }
+      }
+
+      fprintf(fp, "%d %d 0\n", num_migratables, num_migratables);
+      for(int i = 0; i < stats->n_objs; i++) {
+        if (!stats->objData[i].migratable) continue;
 
       LDObjData &odata = stats->objData[i];
       TaggedVector3D* udata = (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
@@ -99,6 +108,8 @@ void MultistepLB_notopo::work(BaseLB::LDStats* stats)
   }
 
   for(int i = 0; i < stats->n_objs; i++){
+    if (!stats->objData[i].migratable) continue;
+
     LDObjData &odata = stats->objData[i];
     TaggedVector3D* udata = (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
 
@@ -124,6 +135,8 @@ void MultistepLB_notopo::work(BaseLB::LDStats* stats)
   if(numInactiveObjects < 1.0*numActiveObjects) {
     // insignificant number of inactive objects; migrate them anyway
     for(int i = 0; i < stats->n_objs; i++){
+      if (!stats->objData[i].migratable) continue;
+
       LDObjData &odata = stats->objData[i];
       TaggedVector3D* udata =
         (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
@@ -315,6 +328,8 @@ void MultistepLB_notopo::work2(BaseLB::LDStats *stats, int count){
       CkAssert(fp != NULL);
       fprintf(fp, "%d %d 0\n", numobjs, numobjs);
       for(int i = 0; i < numobjs; i++) {
+        if(!stats->objData[i].migratable) continue;
+
     LDObjData &odata = stats->objData[i];
     TaggedVector3D* udata =
       (TaggedVector3D *)odata.getUserData(CkpvAccess(_lb_obj_index));
