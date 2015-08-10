42429898777f5b5f1db9941b3fd8b8d9c32f2562
Tom Quinn <trq@astro.washington.edu>
2015-08-10 12:38:28 -0700

    Main::setupICs() Turn on bSphStep if we turn on bDoGas.

--- ParallelGravity.cpp
+++ ParallelGravity.cpp
@@ -1863,8 +1863,11 @@ void Main::setupICs() {
   if(nTotalSPH > 0 && !param.bDoGas) {
       if(prmSpecified(prm, "bDoGas"))
 	  ckerr << "WARNING: SPH particles present and bDoGas is set off\n";
-      else
+      else {
 	  param.bDoGas = 1;
+          if(!prmSpecified(prm, "bSphStep"))
+              param.bSphStep = 1;
+          }
       }
   getStartTime();
   if(param.nSteps > 0) getOutTimes();
