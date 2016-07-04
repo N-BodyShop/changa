142a756676812a3fea98704bb0257d538d8f86fc
Ben Keller <kellerbw@mcmaster.ca>
2016-07-04 14:29:21 -0400

    Remove noisy divv test

--- starform.cpp
+++ starform.cpp
@@ -294,8 +294,6 @@ GravityParticle *Stfm::FormStar(GravityParticle *p,  COOL *Cool, double dTime,
      */
     if (p->massHot() > 0)  
     return NULL;
-    if (p->divv() >= 0)  
-    return NULL;
 #endif
 
     if(p->fDensity < dOverDenMin || p->fDensity/dCosmoFac < dPhysDenMin)
