60023f4cd389244a0c87ba6eab4af020b25b68a7
Spencer Wallace <scw7@uw.edu>
2016-11-10 16:59:17 -0600

    Call addDelParticles after collision calculations

--- ParallelGravity.cpp
+++ ParallelGravity.cpp
@@ -1986,6 +1986,7 @@ void Main::advanceBigStep(int iStep) {
               treeProxy.buildTree(bucketSize, CkCallbackResumeThread());
               doCollisions(dTime, dTimeSub);
               treeProxy.finishNodeCache(CkCallbackResumeThread());
+              addDelParticles();
               }
 
           startTime = CkWallTimer();
