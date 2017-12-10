4566b019318acc8012744632b17a66bfc89c8b39
Tim Haines <thaines.astro@gmail.com>
2017-12-10 04:35:00 -0600

    Fix possible buffer overflows in calls to sprintf
    Compiling with gcc-7.2 shows calculated string sizes possibly larger
    than buffer size. This also fixes possible stack smashing in
    dumpframe.cpp:dfReadColorMapFile.
    
    Usage of DumpFrameContext.FileName could not be changed without
    altering the layout of DumpFrameContext, so it was left alone.
    
    Change-Id: Ib57f3d868ce0e37251658e9778c88fc4dc66ac82

---
+++ formatted_string.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <string>
+#include <array>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+
+template<typename... Args>
+class formatted_string {
+    static constexpr auto max_len = 32;
+	union arena {
+		std::array<char, max_len> array;
+		std::vector<char> vector;
+		arena() {}
+		~arena() {}
+	};
+	arena storage;
+
+	/// length of string (not including null-terminator)
+    int len;
+
+    bool is_short() const noexcept {
+        return len < max_len;
+    }
+public:
+    formatted_string(char const* format, Args... args) {
+    	static_assert(sizeof...(args) > 0, "Must specify at least one argument");
+		len = std::snprintf(nullptr, 0, format, args...);
+		char *const buffer = [&]() {
+			if(is_short()) {
+				new (&storage.array) decltype(storage.array)();
+				return storage.array.data();
+			} else {
+				new (&storage.vector) decltype(storage.vector)(static_cast<size_t>(len) + 1);
+				return storage.vector.data();
+			}
+		}();
+		std::snprintf(buffer, static_cast<size_t>(len + 1), format, args...);
+    }
+
+    template <typename... Ts>
+    formatted_string(formatted_string<Ts...> &&rhs) {
+    	this->len = rhs.len;
+    	if (is_short()) {
+    		new (&this->storage.array) decltype(this->storage.array)();
+    		std::copy(rhs.storage.array.begin(), rhs.storage.array.end(), this->storage.array.begin());
+    	} else {
+    		new (&this->storage.vector) decltype(storage.vector)(std::move(rhs.storage.vector));
+    	}
+    }
+
+    template <typename... Ts>
+    formatted_string& operator=(formatted_string<Ts...> &&rhs) {
+    	*this = std::move(rhs);
+    	return *this;
+    }
+
+    formatted_string(formatted_string const&) = delete;
+    formatted_string& operator=(formatted_string const&) = delete;
+
+    char const* c_str() const noexcept {
+        return (is_short()) ? storage.array.data() : storage.vector.data();
+    }
+    std::string to_string() const {
+        return c_str();
+    }
+
+    ~formatted_string() {
+    	if(!is_short()) {
+    		storage.vector.~vector();
+    	}
+    }
+};
+
+template<typename ... Args>
+formatted_string<Args...> make_formatted_string(char const *format, Args ... args) {
+    return formatted_string<Args...>(format, args...);
+}
