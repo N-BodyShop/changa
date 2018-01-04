3aae26a7459211c908e169a6f8b8a9e34ddc57d3
Cambridge <liu1274@purdue.edu>
2018-01-04 17:13:11 -0600

    correct the printout of the stacks

--- DataManager.cpp
+++ DataManager.cpp
@@ -1159,7 +1159,7 @@ long long totalTraversedParticles = 0;
       }
 
 #ifdef CAMBRIDGE
-      if (tp->largePhase() && tp->getIndex() != 2) {
+/*      if (tp->largePhase() && tp->getIndex() != 2) {
 //        if (tp->largePhase()) {
         for(int j = 1; j <= numParticles; j++) {
           CkPrintf("(%d)th tp %d th particle: acc.x = %f, acc.y = %f, acc.z = %f, potential = %f.\n", tp->getIndex(), j, tp->myParticles[j].treeAcceleration.x,
@@ -1167,7 +1167,7 @@ long long totalTraversedParticles = 0;
                                                                                                                           tp->myParticles[j].treeAcceleration.z,
                                                                                                                           tp->myParticles[j].potential);
         }
-      }
+      }*/
 
       printf("The total number of nodeInterLocal is %lld\n", totalTraversedNodes);
       printf("The total number of particleInterLocal is %lld\n", totalTraversedParticles);
