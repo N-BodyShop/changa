f9a522db7c724ed78ec9c668b397bc325908b84d
Tim Haines <thaines@astro.wisc.edu>
2018-02-14 23:25:06 -0600

    Remove vestigial COSMO_DEBUG logging in DataManager
    Change-Id: I312f6c559a2394a4e0d06c0a2f8e710b26f66b51

--- DataManager.cpp
+++ DataManager.cpp
@@ -273,9 +273,6 @@ Tree::GenericTreeNode *DataManager::pickNodeFromMergeList(int n, GenericTreeNode
     if (nt == Tree::Internal || nt == Tree::Bucket) {
       // we can use this directly, noone else can have it other than NL
       CkAssert(nUnresolved == 0);
-#if COSMO_DEBUG > 0
-      (*ofs) << "cache "<<CkMyPe()<<": "<<keyBits(gtn[i]->getKey(),KeyBits)<<" using Internal node"<<endl;
-#endif
       pickedIndex = i;
       return gtn[i];
       // no change to the count of replicated nodes
@@ -291,17 +288,11 @@ Tree::GenericTreeNode *DataManager::pickNodeFromMergeList(int n, GenericTreeNode
   if(nUnresolved == 0){
     CkAssert(pick < 0);
     // only NonLocal (or Empty). any is good
-#if COSMO_DEBUG > 0
-    (*ofs) << "cache "<<CkMyPe()<<": "<<keyBits(gtn[0]->getKey(),KeyBits)<<" using NonLocal node"<<endl;
-#endif
     pickedIndex = 0;
     return gtn[0];
   }
   else{
     // multiple boundary nodes: return anyone of them
-#if COSMO_DEBUG > 0
-    (*ofs) << "cache "<<CkMyPe()<<": "<<keyBits(gtn[pick]->getKey(),KeyBits)<<" using Boundary node"<<endl;
-#endif
     pickedIndex = pick;
     return gtn[pick];
   } 
@@ -356,9 +347,6 @@ Tree::GenericTreeNode *DataManager::buildProcessorTree(int n, Tree::GenericTreeN
     // keep track if all the children are internal, in which case we have to
     // change this node type too from boundary to internal
     bool isInternal = true;
-#if COSMO_DEBUG > 0
-    (*ofs) << "cache "<<CkMyPe()<<": "<<keyBits(newNode->getKey(),KeyBits)<<" duplicating node"<<endl;
-#endif
     nodeTable.push_back(newNode);
     CkVec<Tree::GenericTreeNode*> newgtn;
     // Recurse into common children.
@@ -377,9 +365,6 @@ Tree::GenericTreeNode *DataManager::buildProcessorTree(int n, Tree::GenericTreeN
     }
     if (isInternal) {
       newNode->setType(Internal);
-#if COSMO_DEBUG > 0
-      (*ofs) << "cache "<<CkMyPe()<<": "<<keyBits(newNode->getKey(),KeyBits)<<" converting to Internal"<<endl;
-#endif
     }
     return newNode;
   }
