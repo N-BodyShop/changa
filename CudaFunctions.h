4c61b75715401fb11e42fc7151e6de935d0f05e1
Cambridge <liu1274@purdue.edu>
2018-04-27 19:51:22 -0400

    Add/edit relatd data structure in cuda_typedef.h

--- CudaFunctions.h
+++ CudaFunctions.h
@@ -6,6 +6,18 @@
 #include "HostCUDA.h"
 void TreePieceCellListDataTransferBasic(CudaRequest *data, workRequest *wr);
 void TreePiecePartListDataTransferBasic(CudaRequest *data, workRequest *wr);
+
+#ifdef GPU_LOCAL_TREE_WALK
+__global__ void gpuLocalTreeWalk(
+    CompactPartData *particleCores,
+    VariablePartData *particleVars,
+    CudaMultipoleMoments* moments,
+    int *ilmarks,
+    int totalNumOfParticles,
+    cudatype theta,
+    cudatype thetaMono);
+#endif //GPU_LOCAL_TREE_WALK
+
 __global__ void nodeGravityComputation(
 		CompactPartData *particleCores,
 		VariablePartData *particleVars,
