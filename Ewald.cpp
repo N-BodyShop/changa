19cab270dc3e7274e8ceb86f9cf2580eea0ebe58
Tom Quinn <trq@astro.washington.edu>
2019-05-25 12:33:33 -0700

    Plug Ewald GPU workrequest callback memory leak.
    Change-Id: I699e5e87d24f76c61c769e57be099ae40beb8d09

--- Ewald.cpp
+++ Ewald.cpp
@@ -514,22 +514,19 @@ void TreePiece::EwaldGPU() {
  */
   roData->fInner2 = (cudatype) 1.1e-2*L*L;
 
-  CkCallback *cb; 
   CkArrayIndex1D myIndex = CkArrayIndex1D(thisIndex); 
-  cb = new CkCallback(CkIndex_TreePiece::EwaldGPUComplete(), myIndex, 
-		      thisArrayID); 
+  cbEwaldGPU = new CkCallback(CkIndex_TreePiece::EwaldGPUComplete(), myIndex,
+                              thisArrayID);
 
-  
-  //CkPrintf("[%d] in EwaldGPU, calling EwaldHost\n", thisIndex);
 #ifdef HAPI_INSTRUMENT_WRS
-  EwaldHost(h_idata, (void *) cb, instrumentId, activeRung, largephase); 
+  EwaldHost(h_idata, (void *) cbEwaldGPU, instrumentId, activeRung, largephase);
 #else
   int myLocalIndex;
   for(myLocalIndex = 0; this != dm->registeredTreePieces[myLocalIndex].treePiece;
       myLocalIndex++);
   CkAssert(myLocalIndex < dm->registeredTreePieces.length());
-  
-  EwaldHost(h_idata, (void *) cb, myLocalIndex, largephase); 
+
+  EwaldHost(h_idata, (void *) cbEwaldGPU, myLocalIndex, largephase);
 #endif
 
 #endif
@@ -544,6 +541,7 @@ void TreePiece::EwaldGPUComplete() {
   int largephase = largePhase(); 
   EwaldHostMemoryFree(h_idata, largephase); 
   free(h_idata); 
+  delete cbEwaldGPU;
 
   /* indicate completion of ewald */
   
