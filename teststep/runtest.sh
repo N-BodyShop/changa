f3d15fa0974a9b2c089934e94b593545ccd15787
Jaemin Choi <jchoi157@illinois.edu>
2020-03-08 00:28:54 -0500

    Revert "Update teststep scripts to use jsrun for OLCF Summit"
    This reverts commit c1be136ec2211528a52c6f325f58f4d8539b7b8f.

--- teststep/runtest.sh
+++ teststep/runtest.sh
@@ -3,6 +3,6 @@
 # one dynamical time in the center of the cluster.
 # compare this with numbers in "pkdtest.log"
 
-jsrun -n2 -a1 -c1 -g1 -K1 -r2 ../ChaNGa -v 1 test_pg.param ++ppn 1
+../charmrun +p 2 ../ChaNGa -v 1 test_pg.param ++local
 
 # pkdgrav test_pkd.param
