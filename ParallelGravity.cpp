a620982ae72765a788cebfb3b899d5c20fa99527
brianxchen <66186605+brianxchen@users.noreply.github.com>
2021-01-21 14:18:05 -0800

    Revert "Py"

--- ParallelGravity.cpp
+++ ParallelGravity.cpp
@@ -1877,9 +1877,8 @@ void Main::kick(bool bClosing, int iActiveRung, int nextMaxRung,
 
     double a = csmTime2Exp(param.csm,dTime);
     double startTime = CkWallTimer();
-    double dOrbFreq = sqrt(param.ExternalGravity.dCentMass / pow(param.ExternalGravity.dOrbDist, 3));
     treeProxy.kick(iActiveRung, dKickFac, bClosing, param.bDoGas,
-                   param.bGasIsothermal, dOrbFreq, param.dMaxEnergy, duKick,
+                   param.bGasIsothermal, param.dMaxEnergy, duKick,
                    (param.dConstGamma-1), param.dThermalCondSatCoeff/a,
                    param.feedback->dMultiPhaseMaxTime,
                    param.feedback->dMultiPhaseMinTemp,
