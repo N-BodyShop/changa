13cd3ba0ed42388e7522c31bf94c8d83be53e822
Tom Quinn <trq@astro.washington.edu>
2024-07-18 16:38:01 -0700

    Fix merge conflict.

--- ParallelGravity.h
+++ ParallelGravity.h
@@ -791,8 +791,6 @@ class TreePiece : public CBase_TreePiece {
    std::vector<double> savedPhaseLoad;
    std::vector<unsigned int> savedPhaseParticle;
 private:
-   /// number of active particles on the last active rung for load balancing
-   unsigned int nPrevActiveParts;
    /// temporary accumulator for phase load information during domain decomposition
    std::vector<double> savedPhaseLoadTmp;
    /// temporary accumulator for phase particle counts during domain decomposition
