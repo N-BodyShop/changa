02047dd1f764da7c6652d108c889b431ceec3351
Ben Keller <malzraa@gmail.com>
2025-01-14 09:24:53 -0600

    Fixed memory access bug in LW table cleanup

--- lymanwerner.c
+++ lymanwerner.c
@@ -51,10 +51,10 @@ void lwInitData(LWDATA *lwd)
 void LymanWernerTableFinalize(LWDATA *lwd)
 {
     int i;
-    if (lwd->lwLuminosity != NULL) free(lwd->lwLuminosity);
     for(i=0; i<lwd->nrows; i++){
         if (lwd->lwLuminosity[i] != NULL) free(lwd->lwLuminosity[i]);
     }
+    if (lwd->lwLuminosity != NULL) free(lwd->lwLuminosity);
     free(lwd);
 }
 
